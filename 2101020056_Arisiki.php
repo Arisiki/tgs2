11dacffa8750d7df77b799ed76b83894dd12d242
Arisiki <151501819+Arisiki@users.noreply.github.com>
2023-11-27 23:57:10 +0800

    Add files via upload

---
+++ 2101020056_Arisiki.php
@@ -0,0 +1,12 @@
+<?php
+
+    $server     = "localhost";
+    $user       = "root";
+    $password   = "";
+    $database   = "2101020056_Arisiki";
+
+    $con = mysqli_connect($server,$user,$password,$database)
+
+?>
+
+
